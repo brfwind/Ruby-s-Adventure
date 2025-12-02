f553487bb0ee9c3b3590fbb4a0565b7fac7c3006
brfwind <19172405320@163.com>
2025-12-02 22:48:00 +0800

    feat:功能场景大完善
    游戏主场景的搭建
    机器人动画的完善
    机器人修好后的状态逻辑
    人物发射齿轮的流程制作
    各种粒子特效的制作
    Cinemachine的使用
    空气墙的添加

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,28 +7,24 @@ public class EnemyController : MonoBehaviour
     public float speed = 2.0f;
     private Rigidbody2D rb;
     private Animator animator;
+    private ParticleSystem smokeEffect;
 
     [Header("自动移动")]
     public bool horizontal; //移动轴向
     private int dir = 1; //移动方向
     public float changeTime = 3.0f; //方向改变时间间隔
     private float timer; //计时器
+    private bool isBroken; //是否故障
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        smokeEffect = GetComponentInChildren<ParticleSystem>();
         timer = changeTime;
+        isBroken = true;
 
-        if (horizontal)
-        {
-            animator.SetFloat("MoveY", 1);
-            animator.SetFloat("Dir", dir);
-        }
-        else
-        {
-            animator.SetFloat("MoveY", -1);
-            animator.SetFloat("Dir", dir);
-        }
+        PlayMoveAnimation();
     }
 
     //转向计时器
@@ -46,6 +42,11 @@ public class EnemyController : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (!isBroken)
+        {
+            return;
+        }
+
         Vector2 position = transform.position;
 
         if (horizontal)
@@ -68,5 +69,31 @@ public class EnemyController : MonoBehaviour
         {
             ruby.ChangeHealth(-1);
         }
+    }
+
+    private void PlayMoveAnimation()
+    {
+        if (horizontal)
+        {
+            animator.SetFloat("MoveY", 1);
+            animator.SetFloat("Dir", dir);
+        }
+        else
+        {
+            animator.SetFloat("MoveY", -1);
+            animator.SetFloat("Dir", dir);
+        }
+    }
+
+    public void Fix()
+    {
+        isBroken = false;
+
+        //不再发生碰撞监测和效果
+        rb.simulated = false;
+
+        animator.SetTrigger("Fixed");
+
+        smokeEffect.Stop();
     }
 }
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class RubyController : MonoBehaviour
 {
@@ -17,9 +18,16 @@ public class RubyController : MonoBehaviour
     private bool isInvincible;
     private float invincibleTimer; //计时器
 
+    [Header("动画相关")]
+    private Vector2 lookDirection = new Vector2(1, 0);
+    private Animator animator;
+
+    public GameObject projectilePrefab;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         currentHealth = maxHealth;
         invincibleTimer = timeInvincible;
         isInvincible = true;
@@ -39,6 +47,11 @@ public class RubyController : MonoBehaviour
         }
 
         Debug.Log(currentHealth);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Launch();
+        }
     }
 
     void FixedUpdate()
@@ -46,9 +59,22 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        //若当前玩家输入的某个轴向值不为0
+        if (!Mathf.Approximately(move.x, 0) || !Mathf.Approximately(move.y, 0))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        //移动
         Vector2 position = transform.position;
-        position.x += speed * horizontal * Time.deltaTime;
-        position.y += speed * vertical * Time.deltaTime;
+        position += speed * move * Time.deltaTime;
 
         rb.MovePosition(position);
     }
@@ -60,9 +86,9 @@ public class RubyController : MonoBehaviour
         {
             //如果在无敌状态
             //不再执行下面的所有代码
-            if(isInvincible)
+            if (isInvincible)
                 return;
-            
+
             //如果无敌状态过期了，且收到了伤害
             isInvincible = true;
             invincibleTimer = timeInvincible;
@@ -70,5 +96,33 @@ public class RubyController : MonoBehaviour
 
         //ps：以上的代码除非因为在无敌时间而进入return，下面的代码还是会正常执行
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+    }
+
+    //发射子弹
+    private void Launch()
+    {
+        //生成一个子弹预制体
+        GameObject projectileObject = Instantiate(projectilePrefab, rb.position, Quaternion.identity);
+        //获得该预制体上的脚本
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+
+        //让子弹只能朝上下左右四个方向发射
+        //Mathf.Sign返回1或-1
+        Vector2 pushDir;
+        if (Mathf.Abs(lookDirection.x) > Mathf.Abs(lookDirection.y))
+        {
+            // 左右方向
+            pushDir = new Vector2(Mathf.Sign(lookDirection.x), 0);
+        }
+        else
+        {
+            // 上下方向
+            pushDir = new Vector2(0, Mathf.Sign(lookDirection.y));
+        }
+
+        //调用脚本里的Launch()方法
+        projectile.Launch(pushDir, 300);
+        //设置动画条件
+        animator.SetTrigger("Launch");
     }
 }
